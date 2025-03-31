9c1a4bb7e730942f6a69565949ad7fcfb8eb113a
Adrian <enevadrian@gmail.com>
2025-03-31 18:41:31 +0300

    Added searchFood to models section (although not really an AI model it still functions similarly in terms of code synxtax)

--- routes/models.ts
+++ routes/models.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import checkUsernameNSFW from '../services/models/checkUsernameNSFW';
 import generateWorkout from '../services/models/generateWorkout';
 import scanImage from '../services/models/checkImageNSFW';
+import fetchFoodData from '../services/models/searchFood';
 const modelsRouter = express.Router();
 
 modelsRouter.get('/', (req, res) => {
@@ -70,6 +71,26 @@ modelsRouter.post('/generateWorkout', async (req, res) => {
     } catch (error) {
         res.status(500).json({ error: 'An error occurred while generating the workout plan' });
     }
+});
+
+// Returns list of foods matching search query
+// Example - search: "Apple"
+// results: "Apple", "Apple pie", "Apple Something", "Apple another something" and so on...
+modelsRouter.get('/searchFood', async (req, res) => {
+
+    const searchQuery = req.query.searchQuery as string;
+
+    try {
+        // Result returns array full of objects
+        const result = await fetchFoodData(searchQuery);
+
+        // If {result} was passed it would wrap that array of objects all into one big object
+        // So result is just passed raw instead of having to call data.result from the frontend
+        res.json(result);
+    } catch (error) {
+        res.status(400).json({ error: 'Invalid URL provided' });
+    }
+
 });
 
 export default modelsRouter;
---
+++ services/models/searchFood.ts
@@ -0,0 +1,33 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+const EDAMAM_APP_ID = process.env.BACKEND_EDAMAM_APP_ID;
+const EDAMAM_APP_KEY = process.env.BACKEND_EDAMAM_APP_KEY;
+
+// Returns list of foods matching search query
+// Example - search: "Apple"
+// results: "Apple", "Apple pie", "Apple Something", "Apple another something" and so on...
+const fetchFoodData = async (search: string) => {
+
+    console.log('Searching query in food database: ', search)
+
+    try {
+        const response = await fetch(
+            `https://api.edamam.com/api/food-database/v2/parser?ingr=${search}&app_id=${EDAMAM_APP_ID}&app_key=${EDAMAM_APP_KEY}`
+        );
+        
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        
+        const data = await response.json();
+        console.log('Foods successfuly retreived from database');
+        return data.hints;
+
+    } catch (error) {
+        console.error('Error fetching food data:', error);
+        throw error; // Rethrow error to handle it outside
+    }
+};
+
+export default fetchFoodData;
